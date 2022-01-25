5ff6ea6f074bbd477b8a5b2ce5832c470cd646e7
Christos Constantinides <christos@nona.digital>
2022-01-25 18:34:17 +0200

    feat: init smart contract logic

---
+++ contracts/NFT.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract NFT is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds; // to increment with a unique identifier for tokens
+    address contractAddress; // address for marketplace we want NFT to be able to interact with to change ownership of contract
+
+    constructor(address marketplaceAddress) ERC721("Metaverse Tokens", "METT") {
+        contractAddress = marketplaceAddress;
+    }
+
+    function createToken(string memory tokenURI) public returns (uint) {
+        _tokenIds.increment();
+        uint256 newITemId = _tokenIds.current();
+
+        _mint(msg.sender, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+        setApprovalForAll(contractAddress, true); // gives it the approval to transact tokens with users from outside contracts
+        return newItemId;
+    }
+}
